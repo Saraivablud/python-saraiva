a12a04094d8f02e395847989c55058c4ab3132fa
Saraivablud <matheus.claudiano.oliveira@escola.pr.gov.br>
2025-05-13 11:31:43 -0300

    capitulo 1 exemplo 1

---
+++ pythonProject2 saraiva/capitulo 1/exemplo1operadoresaritmeticos.py
@@ -0,0 +1,18 @@
+#Operador de soma
+soma = 1+2
+
+#operador de subtraçao
+
+
+subtracao = 2-1
+
+#operador de mutiplicaçao
+mutiplicaçao = 1*3
+
+
+#operador de divisao
+
+divisao = 2/1
+
+#operador de exponenciaçao(potencia)
+exponenciacao = 2**1
