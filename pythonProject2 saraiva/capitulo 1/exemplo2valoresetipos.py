e515659011f3e62094b70061f7c550f36e39f99e
Saraivablud <matheus.claudiano.oliveira@escola.pr.gov.br>
2025-05-20 11:36:28 -0300

    capitulo 1 exemplo 2

---
+++ pythonProject2 saraiva/capitulo 1/exemplo2valoresetipos.py
@@ -0,0 +1,16 @@
+## tipo inteiro : 1,2,3 ....... -2,-1......
+inteiro = 2
+print(type(inteiro))
+#tipo real ou ponto flutuante float 1.3, 0.6 -3.456....
+
+float = 1.3
+print(type(float))
+#caracteres
+char1 = 'a'
+print(type(char1))
+char2 = '2'
+print(type(char2))
+#string conjunto de caracter
+
+stringNome ="Jose valin"
+print(type(stringNome))
